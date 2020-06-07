2012ee40d528d3a9d5fa9f6b1468e80922459569
rong <rong@test.com>
2020-06-07 11:28:44 +0800

    第二次提交

--- src/demo1.java
+++ src/demo1.java
@@ -1,5 +1,8 @@
 public class demo1 {
     public static void main(String[] args) {
-
+        System.out.println("dededede1");
+        System.out.println("dededede2");
+        System.out.println("dededede3");
+        System.out.println("dededede4");
     }
 }
